d2d8728b405474c03392f4f6e4065752c892fd4a
Peter Chu <cgpchu@uwaterloo.ca>
2014-08-16 00:50:46 -0400

    (feature/characterCount) Added live character count which adapts to used characters for user input

--- src/chu/ForCHUApps/tweetoffline/ConfirmDialogFragment.java
+++ src/chu/ForCHUApps/tweetoffline/ConfirmDialogFragment.java
@@ -4,39 +4,58 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.AlertDialog.Builder;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.IntentFilter;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ConfirmDialogFragment extends DialogFragment{
 
 	private static InputFilter[] filterArray = new InputFilter[1];
+	// Each text message is limited to 160 characters
 	private static int MAX_SMS_MESSAGE_LENGTH = 160;
+	private static int usedChars = 0;
 
 	private EditText contents;
+	private TextView characterCount;
+	
+	private final TextWatcher mTextEditorWatcher = new TextWatcher() {
+		
+		@Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        	// Set the count to the number of characters the user is still able to enter
+           characterCount.setText(String.valueOf(160 - usedChars - s.length()));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+	};
 
 	public interface YesNoListener {
 		void onYes(ConfirmDialogFragment confirmDialogFragment);
 		void onNo();
 	}
 
-	public static ConfirmDialogFragment newInstance(String message, boolean requiredInput) {
+	public static ConfirmDialogFragment newInstance(String message, boolean requiredInput, int usedCharacters) {
 		ConfirmDialogFragment frag = new ConfirmDialogFragment();
 		Bundle args = new Bundle();
 		args.putString("message", message);
 		args.putBoolean("requiredInput", requiredInput);
 		frag.setArguments(args);
-		filterArray[0] = new InputFilter.LengthFilter(MAX_SMS_MESSAGE_LENGTH);
+		// Set max length on the message the user can enter
+		filterArray[0] = new InputFilter.LengthFilter(MAX_SMS_MESSAGE_LENGTH - usedCharacters);
+		usedChars = usedCharacters;
 		return frag;
 	}
 
@@ -57,11 +76,15 @@ public class ConfirmDialogFragment extends DialogFragment{
 		builder.setMessage(message);
 		if(requiresInput)
 		{
+			// Set up layout for input box and set the alert dialog builder to the view
+			
 			LayoutInflater i = getActivity().getLayoutInflater();
 			View view = i.inflate(R.layout.dialog_input, null);
 			contents = (EditText) view.findViewById(R.id.dialogInput);
 			contents.setFilters(filterArray);
-			builder.setView(contents);
+			characterCount = (TextView) view.findViewById(R.id.characterCount);
+			characterCount.setText(String.valueOf(160 - usedChars));
+			contents.addTextChangedListener(mTextEditorWatcher);
 			builder.setView(view);
 			contents.requestFocus();
 		}
@@ -82,6 +105,7 @@ public class ConfirmDialogFragment extends DialogFragment{
 			}
 		});
 		Dialog dialog= builder.create();
+		// Show keyboard for user input immediately
 		dialog.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 		return dialog;
 	}
